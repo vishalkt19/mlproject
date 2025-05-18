03af02de3b63e0bde41383cc90f48de71ea29f4b
vishalkt19 <vishalkt19@gmail.com>
2025-05-18 18:45:50 +0530

    Pipelines built

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,5 +1,8 @@
 import os
 import sys
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.abspath(os.path.join(current_dir, "..", ".."))
+sys.path.append(project_root)
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
